abb753974eed2e4f82d08f1259e7cd73be163c14
chamiShavishka1 <chamishavishka5@gmail.com>
2024-06-13 12:54:12 +0530

    allCommit

---
+++ db/DB.js
@@ -0,0 +1,61 @@
+export let Customers = [
+    {
+        custId : 'C01',
+        custName : 'Chami',
+        custAddress : 'Ambalantota',
+        custSalary : '35000'
+    } ,
+
+    {
+        custId : 'C02',
+        custName : 'Shavishka',
+        custAddress : 'Hambantota',
+        custSalary : '25000'
+    },
+
+    {
+        custId : 'C03',
+        custName : 'Chathurika',
+        custAddress : 'Galle',
+        custSalary : '35000'
+    },
+    {
+        custId : 'C04',
+        custName : 'Nethuka',
+        custAddress : 'Matara',
+        custSalary : '25000'
+    }
+];
+export let Items = [
+    {
+        itemId : 'I01',
+        itemName : 'Pen',
+        itemQty : 10,
+        itemPrice : 3
+    },
+    {
+        itemId : 'I02',
+        itemName : 'Book',
+        itemQty : 17,
+        itemPrice : 7
+    },
+    {
+        itemId : 'I02',
+        itemName : 'Bag',
+        itemQty : 14,
+        itemPrice : 8
+    },
+    {
+        itemId : 'I02',
+        itemName : 'Pencil',
+        itemQty : 14,
+        itemPrice : 6
+    },
+    {
+        itemId : 'I02',
+        itemName : 'Eresar',
+        itemQty : 15,
+        itemPrice : 5
+    }
+];
+export let Orders = [];
